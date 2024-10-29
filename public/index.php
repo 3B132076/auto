1af47a1ca3f2f0518442709d70db65da0852b468
steshun <steven921107@yahoo.com.tw>
2024-10-29 23:54:57 +0800

    Change logger name to WISD in public/index.php

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-// 創建一個 Logger 實例
-$logger = new Logger('my_logger');
+// 創建一個 Logger 實例，名稱為 WISD
+$logger = new Logger('WISD');
 $logger->pushHandler(new StreamHandler(__DIR__ . '/../log/app.log', Logger::DEBUG));
 
 // 寫入日誌訊息
